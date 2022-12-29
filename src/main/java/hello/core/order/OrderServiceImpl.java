d1cc2fa743d710dc47ed4d069e70b8d8dac93001
simhani1 <simhani1@gmail.com>
2022-12-30 03:12:26 +0900

    feat: 주문과 할인 도메인

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountpolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountpolicy();  // 할인 정책 변경 시 구현체만 변경하면 된다.
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
