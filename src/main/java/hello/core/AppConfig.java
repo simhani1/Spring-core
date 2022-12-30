ee07f2ebd5df385d7be9d131bfb8b6167cc0184e
simhani1 <simhani1@gmail.com>
2022-12-31 06:24:18 +0900

    refactor: Appconfig 구현 객체 명시 작업

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountpolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -13,12 +14,21 @@ public class AppConfig {
     // 애플리케이션의 실제 동작에 필요한 '구현 객체'를 생성한다.
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    // 다음과 같이 리팩토링을 하면 현재 memberRepository 역할에 어떤 객체가 들어가서 구현을 하는지 명확하게 파악할 수 있게 된다.
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountpolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountpolicy();
     }
 }
