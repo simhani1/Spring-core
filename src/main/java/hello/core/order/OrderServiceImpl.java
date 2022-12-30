4cfc0d9954ab4e4843cb02b70e1f7ed9ce16ebc0
simhani1 <simhani1@gmail.com>
2022-12-31 05:40:14 +0900

    feat: 할인 정책 변경(DIP, OCP 위배)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountpolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountpolicy();  // 할인 정책 변경 시 구현체만 변경하면 된다.
+    // private final DiscountPolicy discountPolicy = new FixDiscountpolicy();  // 할인 정책 변경 시 구현체만 변경하면 된다.
+     private final DiscountPolicy discountPolicy = new RateDiscountPolicy();  // 할인 정책 변경은 수월하다. 하지만 현재 Service 코드에 수정이 발생하므로 OCP에 위배된다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
