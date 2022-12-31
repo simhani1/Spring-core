dc2748dd533049be8e6f1bb49064fb86ab41577e
simhani1 <simhani1@gmail.com>
2022-12-31 17:13:16 +0900

    refactor: 자바에서 스프링으로 변환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,27 +8,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
-
     // Application에 대한 환결설정을 담당하는 클래스
     // 애플리케이션의 실제 동작에 필요한 '구현 객체'를 생성한다.
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // 다음과 같이 리팩토링을 하면 현재 memberRepository 역할에 어떤 객체가 들어가서 구현을 하는지 명확하게 파악할 수 있게 된다.
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();  // 할인 정책 변경
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,17 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        // MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);  // 스프링 컨테이너에 필요한 정보들을 알아서 저장하여 객체를 생성해준다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);  // 이름, 반환 타입 순서, 이때 이름은 메서드명과 동일하다.
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
