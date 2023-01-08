104dfb3ad8bc17cc24651452315b9f2eb8d20575
simhani1 <simhani1@gmail.com>
2023-01-09 01:04:06 +0900

    feat: 컴포넌트 스캔 위치 지정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)  // 컴포넌트 스캔 시 제외시킬 타입을 지정하는 명령어
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)  // 컴포넌트 스캔 시 제외시킬 타입을 지정하는 명령어
 )
 public class AutoAppConfig {
 
