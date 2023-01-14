7774fbae905c274333f597acf323836ee2ac167d
simhani1 <simhani1@gmail.com>
2023-01-15 00:04:52 +0900

    test: 생명주기 설명을 위한 예시 테스트

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCylcleTest.java
@@ -0,0 +1,30 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCylcleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        // AplicationContext를 close하는 일이 없기 때문에 이것의 하위 인터페이스로 선언해야 한다.
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+
+}
