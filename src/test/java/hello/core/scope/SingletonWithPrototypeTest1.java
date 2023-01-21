3263e050ff3a329b6752c0896b171d4eeb1783a4
simhani1 <simhani1@gmail.com>
2023-01-21 17:13:45 +0900

    feat: 웹스코프

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,12 +1,14 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,20 +31,18 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);  // 이미 프로토타입 빈이 싱글톤 객체 생성 시점에 주입이 되어버린 상태로 유지된다.
+        assertThat(count2).isEqualTo(1);  // 이미 프로토타입 빈이 싱글톤 객체 생성 시점에 주입이 되어버린 상태로 유지된다.
         // 즉 주입 받을때 한번 호출된다는 것이다. 매번 새롭게 프로토타입 빈이 호출된다는 것이 아니라는 것을 알아야 한다.
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;  // 생성시점에 주입이 되어 버린다.
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;  // JSR-330 Provider
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();  // 함수가 호출되는 시점에 프로토타입 빈을 요청할 수 있다.
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
